382aee6ceda1d961fb8b92f64d363bf41f62e427
SuneKR <SuneKRmail@gmail.com>
2024-02-19 14:20:50 +0100

    Completed first days assignments

---
+++ 1-intro/1-Assignments/1-Assignments/Program.cs
@@ -0,0 +1,157 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _1_Assignments
+{
+    internal class Program
+    {
+        static void ExerciseOne()
+        {
+            Console.WriteLine("Start of Exercise 1");
+
+            /*
+            --- Exercise 1 ---
+            a. Assign a message to a variable and then print that message.
+
+            b. Assign a message to a variable, and print that message. 
+               Then change the value of the variable to a new message, and print the new message.
+             */
+
+            string ExerOneA = "I have created a mesasge for exercise 1A";
+            Console.WriteLine(ExerOneA);
+
+            string ExerOneB = "I have created a mesasge for exercise 1B";
+            Console.WriteLine(ExerOneB);
+            ExerOneB = "I have changed the mesasge for exercise 1B";
+            Console.WriteLine(ExerOneB);
+        }
+
+        static void ExerciseTwo()
+        {
+            Console.WriteLine("Start of Exercise 2");
+
+            /*
+            --- Exercise 2 ---
+            a. Use a variable to represent a person's name, and print a message to that person. 
+               Your message should just be something simple, such as "Greetings name, are you ready for an adventure?"
+
+            b. Use a variable to represent a person's name, and then print that person's name in
+               lowercase and uppercase
+
+            c. Find a cool quote you like. Print the quote and the name of its author.
+               The output has to include quotation marks!
+
+            d. Repeat 2-c, but this time represent the famous person's name using a variable called famous_person.
+               Then compose your message, and represent it with a new variable called message. Print your message!
+
+            e. Use a variable to represent a person's name, but include some whitespace in the beginning and the end of the name.
+               Print the name once with the whitespace, then three more times stripping the left, right, and total whitespace.
+             */
+
+            string NamedPerson = "[Authentic Human Name]";
+            string ExerTwoA = $"I have created a greating to { NamedPerson } for exercise 2A";
+            Console.WriteLine(ExerTwoA);
+
+            Console.WriteLine("Printing name in lower and upper case for exercise 2B");
+            Console.WriteLine(NamedPerson.ToLower());
+            Console.WriteLine(NamedPerson.ToUpper());
+
+            Console.Write("Exercise 2C:");
+            Console.WriteLine("\"Help me, Obi-Wan Kenobi. You’re my only hope.\" - Captain Picard");
+
+            Console.Write("Exercise 2D:");
+            string DifferentWrongPerson = "Second-class technician Rimmer";
+            Console.WriteLine($"\"Help me, Obi-Wan Kenobi. You’re my only hope.\" - { DifferentWrongPerson }");
+
+            Console.WriteLine("Exercise 2E: Different trims of Name with whitespace (none, left, right, both)");
+            string WhitespaceName = "   Paul Atreides   ";
+            Console.WriteLine($"[{ WhitespaceName }]");
+            Console.WriteLine($"[{ WhitespaceName.TrimStart() }]");
+            Console.WriteLine($"[{ WhitespaceName.TrimEnd() }]");
+            Console.WriteLine($"[{ WhitespaceName.Trim() }]");
+        }
+
+        static void ExerciseThree()
+        {
+            Console.WriteLine("Start of Exercise 3");
+
+            /*
+            --- Exercise 3 ---
+            a. Write addition, subtraction, multiplication, and division that each result in the number 26.
+               Print four lines to show the results.
+
+            b. Use a variable to represent your favourite number.
+               Then, using that variable, create a message that reveals your favourite number - print it!
+
+            c. One kilometer is approximately 0.62 miles.
+               Use a variable to represent your daily travel distance in kilometers.
+               Write code to print your daily travel distance in miles.
+             */
+
+            Console.WriteLine("Exercise 3A:");
+            Console.WriteLine($"7 + 19 = { 7 + 19 }");
+            Console.WriteLine($"48 - 22 = { 48 - 22 }");
+            Console.WriteLine($"2 * 13 = { 2 * 13 }");
+            Console.WriteLine($"78 / 3 = { 78 / 3}");
+
+            Console.WriteLine("Exercise 3B:");
+            int FavouriteNumber = 13;
+            Console.WriteLine($"People might think it's unlucky, but { FavouriteNumber } is my favourite number.");
+
+            Console.WriteLine("Exercise 3C:");
+            double dailyTravelInKm = 5.78;
+            Console.WriteLine($"Daily travel in kilometers: { dailyTravelInKm } km.");
+            Console.WriteLine($"Daily travel in miles: { (dailyTravelInKm * 0.62).ToString("G3") } miles.");
+            //Console.WriteLine($"Daily travel in miles: { dailyTravelInKm * 0.62 } miles.");
+
+        }
+
+        static void Main(string[] args)
+        {
+            /*
+            --- Exercise 1 ---
+            a. Assign a message to a variable and then print that message.
+
+            b. Assign a message to a variable, and print that message. 
+               Then change the value of the variable to a new message, and print the new message.
+
+
+            --- Exercise 2 ---
+            a. Use a variable to represent a person's name, and print a message to that person. 
+               Your message should just be something simple, such as "Greetings name, are you ready for an adventure?"
+
+            b. Use a variable to represent a person's name, and then print that person's name in
+               lowercase and uppercase
+
+            c. Find a cool quote you like. Print the quote and the name of its author.
+               The output has to include quotation marks!
+
+            d. Repeat 2-c, but this time represent the famous person's name using a variable called famous_person.
+               Then compose your message, and represent it with a new variable called message. Print your message!
+
+            e. Use a variable to represent a person's name, but include some whitespace in the beginning and the end of the name.
+               Print the name once with the whitespace, then three more times stripping the left, right, and total whitespace.
+
+
+            --- Exercise 3 ---
+            a. Write addition, subtraction, multiplication, and division that each result in the number 26.
+               Print four lines to show the results.
+
+            b. Use a variable to represent your favourite number.
+               Then, using that variable, create a message that reveals your favourite number - print it!
+
+            c. One kilometer is approximately 0.62 miles.
+               Use a variable to represent your daily travel distance in kilometers.
+               Write code to print your daily travel distance in miles.
+             */
+
+
+            ExerciseOne();
+            Console.WriteLine("");
+            ExerciseTwo();
+            Console.WriteLine("");
+            ExerciseThree();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1-intro/OurFirstApp/OurFirstApp/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Net.WebSockets;
+
+namespace OurFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //19 + 7;
+            //Console.WriteLine("Hello, World!");
+
+            // "cw" + tab er en genvej til Console.WriteLine();
+            //Console.WriteLine(19+7);
+
+            // sjov med varialer
+            // Varable is defined as a named place in memory
+            int x = 19 + 7;
+            // microsoft kalder int er en indbygget type
+            Console.WriteLine(x);
+
+            string name = "Sunam!";
+            Console.WriteLine(name);
+
+            string y = "12";
+            x = 14;
+            Console.WriteLine(y);
+            //Dette virker ikke. Så får man blot en adere string (concatenation)
+            Console.WriteLine(y+x);
+
+            // bool shorthand for Boolean. Men per tradition, så kalder vi bool i c#
+            bool b = false;
+            bool c = true;
+
+            Console.WriteLine(12 < 3);
+            // resultere i en bool.
+
+            // != ikke ens
+            // == ens
+            Console.WriteLine(b != c);
+            Console.WriteLine(b == c);
+
+            //Allowed variable names
+            //lowerCamelCase
+            //SnakeCasing
+            // starting with underscore
+            //include nummbers (don't start with it)
+
+
+            // Not allowed variable names
+            // to start with number
+            // no white space in variables names
+
+            // Variabler skal gerne give mening.
+            // Gerne, korte men sigende navne
+            // 'name' er bedre end 'n'
+            // 'student_name' er bedre end 's_n'
+            // 'name_length' er bedre end 'length_of_persons_name'
+
+            // type conversion
+            int i = 18;
+            string nan = "26";
+
+            string new_var = i.ToString();
+            int new_int = int.Parse(nan);
+
+
+            string navn = "Mark";
+            int alder = 34;
+            Console.WriteLine(navn + " er " + alder + " år gammel.");
+            Console.WriteLine($"{ navn } er { alder } år gammel");
+            Console.WriteLine("Her er et citat: \"Noget klogt\"");
+
+            string my_string = "Mark bor i Mordor";
+            //printer den 6. karakter fr astarten af.
+            Console.WriteLine(my_string[5]); //indeksering starter på 0
+            Console.WriteLine(my_string.ToLower());
+            Console.WriteLine(my_string.ToUpper());
+
+            Console.Write("Giv mig navnet på din by -> "); //skifter ikke linje
+            string place = Console.ReadLine();
+            Console.WriteLine($"Du har intastet: { place }");
+            Console.WriteLine(!place.StartsWith("m"));
+            // all boolean chaecks kan negeres (tjekke efter falsk) ved at tilføje !
+
+            string new_place = " Aarhus ";
+            Console.WriteLine(place == new_place);
+            Console.WriteLine(place == new_place.Trim());
+            // der findes også TrimEnd() og TrimStart(), som kun beskærer henholdsvis slutningen og starten
+
+            // der findes mange indbyggede typer
+            // vi har gennem int, string og bool
+
+            double tal = 2.62;
+            Console.WriteLine(tal);
+
+            Console.ReadKey();
+        }
+    }
+}
