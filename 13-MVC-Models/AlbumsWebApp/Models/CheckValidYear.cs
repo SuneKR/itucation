24e0f1c55474d8608931ea4611f83ec24878258d
SuneKR <SuneKRmail@gmail.com>
2024-03-06 16:21:15 +0100

    todays lessons and assignments

---
+++ 13-MVC-Models/AlbumsWebApp/Models/CheckValidYear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlbumsWebApp.Models
+{
+    internal class CheckValidYear : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            int year = (int)value;
+            if (year < 1960)
+            {
+                ErrorMessage = "No good album was created before the year 1960"; //this is just wrong
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ 13-MVC-Models/PhotoSharingApplication/Models/Photo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhotoSharingApplication.Models
+{
+    public class Photo
+    {
+        private DateTime createdDate;
+
+        public int PhotoID { get; set; }
+        [Required] public string Title { get; set; }
+        [DisplayName("Picture")] public byte[] PhotoFile { get; set; }
+        public string ImageMimeType { get; set; }
+        [DataType(DataType.MultilineText)] public string Description { get; set; }
+        [DataType(DataType.DateTime)][DisplayName("Created Date")][DisplayFormat(DataFormatString = "{0:MM/dd/yy}")] public DateTime CreatedDate { get => createdDate; set => createdDate = value; }
+        public string UserName { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
