6a4ec9e2f20f808857d582433b903c50984208fb
SuneKR <SuneKRmail@gmail.com>
2024-03-06 08:48:04 +0100

    Updateded 11-assignments with extra map and rename 4-ValueVsReference folder so it aligns with namingschemes.

--- 11-FrontendApp/11-Assignments/Navigationsbar/navigationBar.js
+++ 11-FrontendApp/11-Assignments/Navigationsbar/navigationBar.js
@@ -4,10 +4,16 @@
         .then(response => response.text())
         .then(data => { navigationBar.innerHTML = data; })
         .then(function () {
+            var dropdownToggleA = document.getElementById('myDropdownToggleA');
+            var dropdownA = document.getElementById('myDropdownContainerA');
             var dropdownToggleB = document.getElementById('myDropdownToggleB');
             var dropdownB = document.getElementById('myDropdownContainerB');
             var dropdownToggleM = document.getElementById('myDropdownToggleM');
             var dropdownM = document.getElementById('myDropdownContainerM');
+
+            dropdownToggleA.addEventListener('click', function () {
+                dropdownA.classList.toggle('show');
+            });
 
             dropdownToggleB.addEventListener('click', function () {
                 dropdownB.classList.toggle('show');
@@ -18,6 +24,9 @@
             });
 
             document.addEventListener('click', function (event) {
+                if (!event.target.matches('#myDropdownToggleA')) {
+                    dropdownA.classList.remove('show');
+                }
                 if (!event.target.matches('#myDropdownToggleB')) {
                     dropdownB.classList.remove('show');
                 }
